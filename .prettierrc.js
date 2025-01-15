0b30c6bde3ae83b87bbc45d56c71476b473cc4a3
Flavio <flaviomatheus28013@gmail.com>
2025-01-14 22:14:10 -0300

    initial configuration

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80, //limite de digitção até que a linha se quebre
+  tabWidth: 2, // quantidade de espaços ao clicar tab
+};
