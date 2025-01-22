6979c7782f9be71e02bd62ce0e50a0facbfa29f5
Flavio <flaviomatheus28013@gmail.com>
2025-01-21 21:20:51 -0300

    .

--- src/SRP/main.ts
+++ src/SRP/main.ts
@@ -1,12 +1,22 @@
-import { ShoppingCart } from "./shopping-cart";
+import { Messaging } from './services/messaging';
+import { Order } from './entities/order';
+import { ShoppingCart } from './entities/shopping-cart';
+import { Persistency } from './services/persistency';
+import { Product } from './entities/product';
 
 const shoppingCart = new ShoppingCart();
-shoppingCart.addItem({ name: 'Camiseta', price: 49.90 });
-shoppingCart.addItem({ name: 'Caderno', price: 5.90 });
-shoppingCart.addItem({ name: 'mochila', price: 59.90});
-
+const messagin = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messagin, persistency);
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 19.9));
+shoppingCart.addItem(new Product('mochila', 59.9));
+shoppingCart.addItem({ name: 'Camiseta', price: 49.9 });
+shoppingCart.addItem({ name: 'Caderno', price: 5.9 });
+shoppingCart.addItem({ name: 'mochila', price: 59.9 });
 
 console.log(shoppingCart.items);
 console.log(shoppingCart.total());
-shoppingCart.checkout()
-console.log(shoppingCart.orderStatus);
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
