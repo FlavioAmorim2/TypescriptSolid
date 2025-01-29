3acfa5cbcb03b90f3c766886b6432e4b016e8b41
Flavio <flaviomatheus28013@gmail.com>
2025-01-29 19:54:01 -0300

    .

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+/*
+vantagens e desvantagens do S.O.L.I.D
+
+  Vantagens:
+    - código moldularizado
+    - Código reutilizavel (D.R.Y - Don't repeat self)
+    - Código testavel, baixo acoplamento
+    - Baixo acomp´lamento e alta coesão
+    - Códigoexpansível
+    - Separation of concerns (Separação de conceitos)
+    - Fácil manutenção
+
+  Desvantagens:
+    - complexidade
+    - Quantidade de código digitado aumenta
+    - Tempo de desenvolvimento aumenta bastante
+    - Tenha cuidados com: YAGNI, KISS (YOU AREN'T GONNA NEED IT, KEEP IT SILPLE, STUPID)
+
+
+    */
+
+
+
+
